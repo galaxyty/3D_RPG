2465a8ec94c778f70de11b22e5bfe166bcd4547a
Host <Host>
2022-12-29 19:37:33 +0900

    누락파일 커밋

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -9,5 +9,8 @@ public class GameManager : BaseSingleton<GameManager>
     {
         // 번들 다운로드.        
         BundleManager.Instance.DownloadBundleAsync();
+        
+        PoolManager.Instance.Create<PlayerController>(Constants.kBUNDLE.PLAYER.ToString());
+        PoolManager.Instance.Pop<PlayerController>();
     }
 }
