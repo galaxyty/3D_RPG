f4f12f9c9720f721a98b1ac507ce3657c71d17a5
Host <Host>
2023-01-01 17:44:50 +0900

    불필요한 코드 제거

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,10 +6,7 @@ using BaseRPG_V1;
 public class GameManager : BaseSingleton<GameManager>
 {
     private void Start() 
-    {
-        // 번들 다운로드.        
-        BundleManager.Instance.DownloadBundleAsync();
-        
+    {   
         PoolManager.Instance.Create<PlayerController>(Constants.kBUNDLE.PLAYER.ToString());
         PoolManager.Instance.Pop<PlayerController>();
     }
