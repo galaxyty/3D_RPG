78e58cc0e100d419224e51c2bc2942fc76ad0cd1
Host <Host>
2023-02-08 05:09:35 +0900

    번들갱신 및 불필요한 번들 삭제

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -5,14 +5,39 @@ using BaseRPG_V1;
 
 public class EnemyController : BaseCharacter
 {
+    public override void Hit(int damage)
+    {
+        base.Hit(damage);
+
+        Debug.Log("남은 HP : " + m_Hp);
+    }
+
+    public override void Die()
+    {
+        base.Die();
+        
+        Debug.Log("사망");
+
+        PoolManager.Instance.Push(this);
+    }
+
     private void OnTriggerEnter(Collider other) 
     {
+        // 받은 데미지.
+        int damage = 0;
+
+        // 플레이어한테 피격당할 시.
         if (other.gameObject.tag == Constants.kTAG.Weapon.ToString())
         {
-            var obj = PoolManager.Instance.GetObject<PlayerController>();
+            var player = other.transform.root.GetComponent<PlayerController>();
 
-            if (obj == null)
+            // null 체크.
+            if (player == null)
                 return;
+            
+            damage = player.AttackPower;
         }
+
+        Hit(damage);
     }
 }
