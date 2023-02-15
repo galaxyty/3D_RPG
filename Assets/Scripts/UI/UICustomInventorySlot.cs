fdc69a5d1210d3be6c43acc3794a37c5f11dd8f2
Host <Host>
2023-02-16 08:39:10 +0900

    번들 수정

--- Assets/Scripts/UI/UICustomInventorySlot.cs
+++ Assets/Scripts/UI/UICustomInventorySlot.cs
@@ -2,79 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using BaseRPG_V1;
 
-public class UICustomInventorySlot : BaseInventorySlot
+public class UICustomInventorySlot : UIInventorySlot
 {
-    // 인덱스 텍스트 UI.
-    [SerializeField]
-    protected Text m_TextOfIndex;
-
-    // 아이템 이미지.
-    [SerializeField]
-    protected Image m_ImageOfItem;
-
-    // 아이템 소지 데이터.
-    protected ItemData m_Data;
-
     public override void Initialization()
     {
         base.Initialization();
 
         m_ImageOfItem.sprite = BundleManager.Instance.LoadToSprite(Constants.kBUNDLE.UIInventorySlot.ToString());
-        m_TextOfIndex.gameObject.SetActive(false);
     }
 
     public override void DisposeObject()
     {
         base.DisposeObject();
 
-        m_TextOfIndex.gameObject.SetActive(false);
-        m_TextOfIndex.text = null;
         m_ImageOfItem.sprite = BundleManager.Instance.LoadToSprite(Constants.kBUNDLE.UIInventorySlot.ToString());
-        m_Data = null;
-    }
-
-    // 인벤토리 슬롯 업데이트.
-    public void UpdateUI(ItemData data)
-    {
-        // 데이터 넣기.
-        m_Data = data;
-
-        // null 체크.
-        if (m_Data == null)
-            return;
-
-        // ui 갱신.
-        m_TextOfIndex.gameObject.SetActive(true);
-        m_TextOfIndex.text = m_Data.INDEX.ToString();
-        m_ImageOfItem.sprite = BundleManager.Instance.LoadToSprite(m_Data.INDEX.ToString());
-    }
-
-    // 포션 아이템.
-    private void UsePorsion()
-    {
-        // 포션 아이템.
-        var porsion = TableManager.Instance.GetPorsionData().Find(foundData => foundData.ITEM_INDEX == m_Data.INDEX);
-
-        // null 체크.
-        if (porsion == null)
-            return;
-
-        // 체력 회복.
-        var player = PoolManager.Instance.GetObject<PlayerController>();
-
-        // null 체크.
-        if (player == null)
-            return;
-
-        // 체력 회복.
-        player.CureHP(porsion.HP);
-
-        // 사용한 아이템 제거.
-        player.Inventory.Remove(m_Data);
-
-        DisposeObject();
     }
 
     // 인벤토리 슬롯 버튼.
@@ -84,16 +26,37 @@ public class UICustomInventorySlot : BaseInventorySlot
         if (m_Data == null)
             return;
 
+        // 플레이어 객체 가져옴.
+        var player = PoolManager.Instance.GetObject<PlayerController>();
+
         switch (m_Data.ITEM_TYPE)
         {
             case ItemData.kITEM_TYPE.Weapon:
+
+                // null 체크.
+                if (player == null)
+                    return;
+
+                // 무기 장착.
+                player.UpdateWeapon(m_Data);
+
                 break;
 
             case ItemData.kITEM_TYPE.Porsion:
+            
+                // 포션 사용.
                 UsePorsion();
                 break;
 
             case ItemData.kITEM_TYPE.Armor:
+
+                // null 체크.
+                if (player == null)
+                    return;
+
+                // 방어구 장착.
+                player.UpdateArmor(m_Data);
+
                 break;
         }
     }
