43d93a00b99cf48dc1cf9be7755e46d056784798
Host <Host>
2023-02-28 10:23:54 +0900

    장비창에서 아이템 해제 기능 구현

--- Assets/Scripts/UI/UICustomEquitmentSlot.cs
+++ Assets/Scripts/UI/UICustomEquitmentSlot.cs
@@ -20,6 +20,9 @@ public class UICustomEquitmentSlot : BaseInventorySlot
         base.DisposeObject();
 
         m_ImageOfItem.sprite = BundleManager.Instance.LoadToSprite(Constants.kBUNDLE.UIInventorySlot.ToString());
+
+        m_TextOfIndex.text = "0";
+        m_Data = null;
     }
 
     // 장비창 슬롯 업데이트.
@@ -41,7 +44,43 @@ public class UICustomEquitmentSlot : BaseInventorySlot
     }
 
     public override void OnTouchEvent()
-    {
-        
+    {        
+        if (m_Data == null)
+        {
+            return;
+        }
+
+        var data = TableManager.Instance.GetItemData().Find(foundData => foundData.INDEX == m_Data.ITEM_INDEX);
+
+        if (data == null)
+        {
+            return;
+        }
+
+        var player = PoolManager.Instance.GetObject<PlayerController>();
+
+        if (player == null)
+        {
+            return;
+        }
+
+        // 장착 해제.
+        switch (data.ITEM_TYPE)
+        {
+            case ItemData.kITEM_TYPE.Weapon:
+                // 이미 장착중이면 알아서 착용 해제.
+                player.UpdateWeapon(m_Data);
+                break;
+            
+            case ItemData.kITEM_TYPE.Armor:
+                // 이미 장착중이면 알아서 착용 해제.
+                player.UpdateArmor(m_Data);
+                break;
+            
+            default:
+                break;
+        }
+
+        DisposeObject();
     }
 }
