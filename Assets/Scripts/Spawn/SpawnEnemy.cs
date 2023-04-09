d98ff3903d0691c79b36148bb1701b0af3c8aedf
Host <Host>
2023-04-10 04:08:21 +0900

    스폰시키는 스크립트 프레임워크로 변경

---
+++ Assets/Scripts/Spawn/SpawnEnemy.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using BaseRPG_V1;
+
+public class SpawnEnemy : BaseSpawn
+{
+    // 스폰시킬 몬스터 풀매니저 크리에이트.
+    public override void SpawnCreate()
+    {
+        PoolManager.Instance.Create<EnemyController>(Constants.kMONSTER.Enemy.ToString());
+    }
+
+    // 일정시간마다 크리에이트시킨 몬스터 필드에 생성.
+    public override void SpawnPop()
+    {
+        var obj = PoolManager.Instance.Pop<EnemyController>(transform);
+        obj.spawnEnemy = this;
+        obj.Index = TableManager.Instance.GetMonsterData().Find(foundData => foundData.INDEX == 1).INDEX;
+        m_currentCount++;
+    }
+}
