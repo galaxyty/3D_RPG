8f36f44d1ad9287ec58fccfc847b84b6e5710ed5
Host <Host>
2023-04-14 19:41:41 +0900

    게임시작 함수 내용 변경

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,11 +5,16 @@ using BaseRPG_V1;
 
 public class GameManager : BaseSingleton<GameManager>
 {
-    private void Start() 
+    private void Start() {
+        BundleDownloadManager.Instance.DownloadBundleAsync(Constants.kBUNDLE.Player.ToString(), GameStart);
+    }
+
+    // 게임 시작 함수.
+    public void GameStart()
     {
         // 캐릭터 셋팅.
         PoolManager.Instance.Create<PlayerController>(Constants.kBUNDLE.Player.ToString());
-        PoolManager.Instance.Pop<PlayerController>();        
+        PoolManager.Instance.Pop<PlayerController>();
 
         // 유저 인터페이스 셋팅.
         PoolManager.Instance.Create<UICharacter>(Constants.kBUNDLE.UICharacter.ToString());
