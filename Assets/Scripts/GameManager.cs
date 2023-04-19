c38703aa14ff3e230dde62bc9ca6c83dfc6d207f
Host <Host>
2023-04-19 10:56:38 +0900

    스폰 스크립트 구조 수정

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -11,9 +11,13 @@ public class GameManager : BaseSingleton<GameManager>
         PoolManager.Instance.Create<PlayerController>(Constants.kBUNDLE.Player.ToString());
         PoolManager.Instance.Pop<PlayerController>();
 
+        // 몬스터 스폰 생성.
+        PoolManager.Instance.Create<SpawnEnemy>(Constants.kBUNDLE.SpawnEnemy.ToString());
+        PoolManager.Instance.Pop<SpawnEnemy>();
+
         // 유저 인터페이스 셋팅.
         PoolManager.Instance.Create<UICharacter>(Constants.kBUNDLE.UICharacter.ToString());
-        PoolManager.Instance.Pop<UICharacter>(Constants.kTAG.MainCanvas.ToString());
+        PoolManager.Instance.Pop<UICharacter>(Constants.kTAG.MainCanvas.ToString());        
     }
 
     // GameManager 싱글톤 생성을 위한 함수. (Start 함수 실행을 위해 만들어둠).
